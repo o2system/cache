095e14d77dddb59fd675f72dc59a55eab37742af
Steeven Andrian Salim <steevenz@ymail.com>
2016-12-08 21:14:04 +0700

    Move to PSR-2 & 6

---
+++ src/Abstracts/AbstractAdapter.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * This file is part of the O2System PHP Framework package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author         Steeve Andrian Salim
+ * @copyright      Copyright (c) Steeve Andrian Salim
+ */
+// ------------------------------------------------------------------------
+
+namespace O2System\Cache\Abstracts;
+
+// ------------------------------------------------------------------------
+
+use O2System\Cache\Registries\Config;
+use O2System\Kernel\Spl\Exceptions\Logic\BadFunctionCall\BadPhpExtensionCallException;
+use O2System\Psr\Cache\CacheItemPoolAdapterInterface;
+
+/**
+ * Class AbstractAdapter
+ *
+ * @package O2System\Cache\Abstracts
+ */
+abstract class AbstractAdapter extends AbstractItemPool implements CacheItemPoolAdapterInterface
+{
+    /**
+     * Adapter Platform Name
+     *
+     * @var string
+     */
+    protected $platform;
+
+    /**
+     * Adapter Config
+     *
+     * @var array
+     */
+    protected $config = [ ];
+
+    /**
+     * Adapter Prefix Key
+     *
+     * @var string
+     */
+    protected $prefixKey;
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * AbstractAdapter::__construct
+     *
+     * @param \O2System\Cache\Registries\Config|NULL $config
+     *
+     * @return AbstractAdapter
+     * @throws BadPhpExtensionCallException
+     */
+    public function __construct ( Config $config = null )
+    {
+        if ( isset( $config ) ) {
+            if ( $this->isSupported() ) {
+                $this->connect( $config->getArrayCopy() );
+
+                if ( $config->offsetExists( 'prefixKey' ) ) {
+                    $this->setPrefixKey( $config->prefixKey );
+                }
+            } else {
+                throw new BadPhpExtensionCallException( 'E_MESSAGE_CACHE_UNSUPPORTED_ADAPTER', 0, [ $this->platform ] );
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * AbstractAdapter::setPrefixKey
+     *
+     * Sets item prefix key.
+     *
+     * @param $prefixKey
+     */
+    public function setPrefixKey ( $prefixKey )
+    {
+        $this->prefixKey = rtrim( $prefixKey, ':' ) . ':';
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * AbstractAdapter::getPlatform
+     *
+     * Gets item pool adapter platform name.
+     *
+     * @return string
+     */
+    public function getPlatform ()
+    {
+        return $this->platform;
+    }
+}
---
+++ src/Abstracts/AbstractItemPool.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * This file is part of the O2System PHP Framework package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author         Steeve Andrian Salim
+ * @copyright      Copyright (c) Steeve Andrian Salim
+ */
+// ------------------------------------------------------------------------
+
+namespace O2System\Cache\Abstracts;
+
+// ------------------------------------------------------------------------
+
+use O2System\Kernel\Spl\Exceptions\Logic\InvalidArgumentException;
+use O2System\Psr\Cache\CacheItemInterface;
+use O2System\Psr\Cache\CacheItemPoolInterface;
+
+/**
+ * Class AbstractItemPool
+ *
+ * @package O2System\Cache\Abstracts
+ */
+abstract class AbstractItemPool implements CacheItemPoolInterface
+{
+    /**
+     * Deferred Items Storage
+     *
+     * @var array
+     */
+    private $storage = [ ];
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * CacheItemPoolInterface::deleteItems
+     *
+     * Removes multiple items from the pool.
+     *
+     * @param string[] $keys
+     *   An array of keys that should be removed from the pool.
+     *
+     * @return bool
+     * @throws \O2System\Kernel\Spl\Exceptions\Logic\InvalidArgumentException
+     * @throws \O2System\Psr\Cache\InvalidArgumentException
+     */
+    public function deleteItems ( array $keys )
+    {
+        if ( ! is_array( $keys ) ) {
+            throw new InvalidArgumentException( 'E_HEADER_INVALIDARGUMENTEXCEPTION' );
+        }
+
+        foreach ( $keys as $key ) {
+            if ( $this->deleteItem( $key ) === false ) {
+                return false;
+                break;
+            }
+        }
+
+        return true;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * CacheItemPoolInterface::saveDeferred
+     *
+     * Sets a cache item to be persisted later.
+     *
+     * @param CacheItemInterface $item
+     *   The cache item to save.
+     *
+     * @return bool
+     *   False if the item could not be queued or if a commit was attempted and failed. True otherwise.
+     */
+    public function saveDeferred ( CacheItemInterface $item )
+    {
+        $this->storage[] = $item;
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * CacheItemPoolInterface::commit
+     *
+     * Persists any deferred cache items.
+     *
+     * @return bool
+     *   True if all not-yet-saved items were successfully saved or there were none. False otherwise.
+     */
+    public function commit ()
+    {
+        $storage = $this->storage;
+
+        foreach ( $storage as $key => $item ) {
+            if ( $this->save( $item ) === true ) {
+                unset( $storage[ $key ] );
+            }
+        }
+
+        if ( count( $storage ) == 0 ) {
+            $this->storage = [ ];
+
+            return true;
+        }
+
+        return false;
+    }
+}
